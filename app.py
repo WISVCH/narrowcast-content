8f8c38aaeac693f6841fae492aa0981e49ea0190
Robert van Dijk <robert@citroenvlinder17.nl>
2023-12-31 12:59:44 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, abort
+import logging
+from logging import Formatter, FileHandler
+
+app = Flask(__name__)
+app.config.from_object('config')
+
+
+
+
+if not app.debug:
+    file_handler = FileHandler('error.log')
+    file_handler.setFormatter(
+        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
+    )
+    app.logger.setLevel(logging.INFO)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info('errors')
+
+# Default port:
+if __name__ == '__main__':
+    app.run()
