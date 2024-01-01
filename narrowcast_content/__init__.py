9e9bf6ceb2dac83e1c3afd0561700fdb7ba2b341
Robert van Dijk <robert@citroenvlinder17.nl>
2024-01-01 14:29:37 +0100

    Clean up init file

--- narrowcast_content/__init__.py
+++ narrowcast_content/__init__.py
@@ -3,44 +3,40 @@ from logging import Formatter, FileHandler
 
 from flask import Flask, request, abort, session
 
+from narrowcast_content.buienradar_graph import buienradar_graph
+from narrowcast_content.combine import combine
+from narrowcast_content.image import image
+from narrowcast_content.pub_timer import pub_timer
+from narrowcast_content.spotify_now_playing import spotify_now_playing
+
 app = Flask(__name__)
 app.config.from_object('config')
 
 if not app.debug:
     file_handler = FileHandler('error.log')
-    file_handler.setFormatter(
-        Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]')
-    )
+    file_handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     app.logger.setLevel(logging.INFO)
     file_handler.setLevel(logging.INFO)
     app.logger.addHandler(file_handler)
     app.logger.info('errors')
 
-from narrowcast_content.buienradar_graph import buienradar_graph
-
+# Add all the blueprints
 app.register_blueprint(buienradar_graph)
-
-from narrowcast_content.spotify_now_playing import spotify_now_playing
-
 app.register_blueprint(spotify_now_playing)
-
-from narrowcast_content.combine import combine
-
 app.register_blueprint(combine)
-
-from narrowcast_content.image import image
-
 app.register_blueprint(image)
-
-from narrowcast_content.pub_timer import pub_timer
-
 app.register_blueprint(pub_timer)
 
+# Parse the tokens
 tokens = app.config['TOKENS'].split()
 
 
 @app.before_request
 def check_auth():
+    """
+    Checks if the client sends a token, and if the token is valid.
+    Puts the token in the session so that it has to be provided only once.
+    """
     if 'token' not in request.args and 'token' not in session:
         abort(401, "Unauthorized")
 
