d18226ed5c1aa309f19d6f2396254c296b729684
Robert van Dijk <robert@citroenvlinder17.nl>
2024-01-01 14:47:09 +0100

    Add comments to config

--- config.py
+++ config.py
@@ -6,13 +6,17 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 # Enable debug mode.
 DEBUG = True
 
-# Secret key for session management. You can generate random strings here:
+# Secret key for session management.
 SECRET_KEY = 'CHANGE_ME'
 
-SPOTIPY_CLIENT_ID='CHANGE_ME'
-SPOTIPY_CLIENT_SECRET='CHANGE_ME'
-SPOTIPY_REDIRECT_URI='http://localhost:5000/spotify_now_playing/api_callback'
+# Spotify API credentials.
+SPOTIPY_CLIENT_ID = 'CHANGE_ME'
+SPOTIPY_CLIENT_SECRET = 'CHANGE_ME'
+SPOTIPY_REDIRECT_URI = 'CHANGE_ME'
 
-PUB_GOOGLE_CALENDAR_ID='CHANGE_ME'
-PUB_GOOGLE_CALENDAR_API_KEY='CHANGE_ME'
-TOKENS='CHANGE_ME CHANGE_ME_TOO'
+# /Pub Calendar information.
+PUB_GOOGLE_CALENDAR_ID = 'CHANGE_ME'
+PUB_GOOGLE_CALENDAR_API_KEY = 'CHANGE_ME'
+
+# Tokens that can be used to access everything.
+TOKENS = 'CHANGE_ME CHANGE_ME_TOO'
