20cee0af166f1965aede7600606c7b0c256b5644
Robert van Dijk <robert@citroenvlinder17.nl>
2024-03-08 12:08:01 +0100

    Increase waitress limits

--- narrowcast_content/waitress_server.py
+++ narrowcast_content/waitress_server.py
@@ -2,4 +2,9 @@ from waitress import serve
 from . import app
 from paste.translogger import TransLogger
 
-serve(TransLogger(app, setup_console_handler=False), host='0.0.0.0', port=8080)
+serve(TransLogger(app, setup_console_handler=False),
+      host='0.0.0.0',
+      port=8080,
+      connection_limit=500,
+      threads=8,
+      )
