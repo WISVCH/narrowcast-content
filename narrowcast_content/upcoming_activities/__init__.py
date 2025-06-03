845598f5035f664572d3b2cfcc709ce7734c0ec7
Robert van Dijk <robert@citroenvlinder17.nl>
2025-06-03 09:47:09 +0200

    Refactor event filtering functions to support events that started in the past

--- narrowcast_content/upcoming_activities/__init__.py
+++ narrowcast_content/upcoming_activities/__init__.py
@@ -42,21 +42,23 @@ def fetch_and_parse_ics(url):
         return []
 
 
-# Function to filter events for the next 7 days
-def filter_events_for_next_n_days(events, n):
+
+'''Function to filter events for the next n days, optionally starting from m days ago'''
+def filter_events(events, past_days, future_days):
     now = datetime.now(timezone.utc)
     start_of_today = now.replace(hour=0, minute=0)
-    end_of_week = (now + timedelta(days=n-1)).replace(hour=23, minute=0)
+    from_date = start_of_today - timedelta(days=past_days)
+    to_date = (now + timedelta(days=future_days-1)).replace(hour=23, minute=0)
 
     return [
         event
         for event in events
-        if start_of_today <= event['start'] < end_of_week
+        if from_date <= event['start'] < to_date
     ]
 
 
 # Function to organize events by day
-def organize_events_by_day(events, n):
+def organize_events_by_day(events, past_days, future_days):
     organized_events = {}
 
     today = datetime.now()
@@ -71,7 +73,7 @@ def organize_events_by_day(events, n):
         if event['end'].date() > event['start'].date() and event['end'].hour >= 6:
             # Add event for each day until the end date, or n days (excluding events ending before 9:00AM the next day)
             current_date = event['start'].date()
-            while current_date <= event['end'].date() and current_date < (today + timedelta(days=n)).date():
+            while current_date <= event['end'].date() and current_date < (today + timedelta(days=past_days+future_days)).date():
                 if current_date.strftime('%Y-%m-%d') not in organized_events:
                     organized_events[current_date.strftime('%Y-%m-%d')] = []
                 current_date_str = current_date.strftime('%Y-%m-%d')
@@ -90,11 +92,15 @@ def organize_events_by_day(events, n):
                 'categories': event['categories'],
             })
 
-    for i in range(n):
+    for i in range(-past_days, future_days):
         current_date = today + timedelta(days=i)
         current_date_str = current_date.strftime('%Y-%m-%d')
         if current_date_str not in organized_events:
             organized_events[current_date_str] = []
+
+    for date in list(organized_events.keys()):
+        if date < (today - timedelta(days=past_days)).strftime('%Y-%m-%d'):
+            del organized_events[date]
 
     return organized_events
 
@@ -115,7 +121,6 @@ def events():
 
     # Fetch and parse ICS data, then organize events
     ics_events = fetch_and_parse_ics(ics_url)
-    filtered_events = filter_events_for_next_n_days(ics_events, days)
-    organized_events = organize_events_by_day(filtered_events, days)
-    # Print the result
+    filtered_events = filter_events(ics_events, 14, days)
+    organized_events = organize_events_by_day(filtered_events, 0, days)
     return organized_events
